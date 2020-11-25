18e3706700719ae1ca1b64ba1da574854b70a2d4
HELIO MARCUS NERY FIDALGO <hmnff@hotmail.com>
2020-11-25 18:17:01 +0000

    final commit
    finish first course.

---
+++ FrogGamePlayground.playground/Pages/Página 1 - Classes e variáveis.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+
+//: # Página 1 - Classes e variáveis
+
+import UIKit
+import PlaygroundSupport
+
+//: ![Imagem](passo1.png)
+//: Escreva aqui a classe do sapo que será usada.
+
+
+
+
+//: ![Imagem](passo2.png)
+//: Crie novos sapos.
+//var frog = Frog()
+
+//: ![Imagem](passo3.png)
+//: Imprima informações de algum sapo criado acima.
+//print(frog.height)
+
+//: ![Imagem](passo4.png)
+/*
+ // Interface que apresenta sapo(s) criado(s)
+class MyScreenViewController : UIViewController {
+
+    override func viewDidLoad() {
+        //let myFrog1 = Frog()
+        //addFrog(frog: myFrog1)
+    }
+
+    func addFrog(frog:Frog){
+        let imageView = UIImageView(image: UIImage(named: frog.nameImageFrog))
+        imageView.frame = CGRect(x: frog.xposition, y: frog.yposition, width: frog.width, height: frog.height)
+        view.addSubview(imageView)
+    }
+
+}
+
+// Código responsável por instanciar uma insterface e aoresentar no liveView
+var viewController = MyScreenViewController()
+PlaygroundPage.current.liveView = viewController
+*/
+
+//: [Próximo](@next)
